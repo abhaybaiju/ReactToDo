35c013d13e6ffb09f7a44e97fac9b0806b118032
abhay.baiju <abhay0198@gmail.com>
2020-05-25 07:52:04 -0700

    Updates

---
+++ precache-manifest.91a004587d8f65c6361a1b74de0567bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df3ba758092a64bb9e5a7f3bfc3abf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "690d601b5d611d4e8c86",
+    "url": "/static/css/main.f31a359a.chunk.css"
+  },
+  {
+    "revision": "675cc1d2a78a44f70b20",
+    "url": "/static/js/2.63134917.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.63134917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690d601b5d611d4e8c86",
+    "url": "/static/js/main.5d2fb046.chunk.js"
+  },
+  {
+    "revision": "759e81ee1e3f967c5c66",
+    "url": "/static/js/runtime-main.66c35898.js"
+  }
+]);
